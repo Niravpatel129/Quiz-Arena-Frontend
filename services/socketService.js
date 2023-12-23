496c42a3f116e412f57cf15001b9a3c4278aeb09
Nirav <niravpatelp129@gmail.com>
2023-12-22 21:55:43 -0500

    Update SOCKET_URL to use WebSocket protocol

--- services/socketService.js
+++ services/socketService.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const SOCKET_URL = IS_PRODUCTION
   ? 'https://quiz-arena-backend-96d07772fd89.herokuapp.com'
-  : 'https://localhost:8001';
+  : 'ws://localhost:8001';
 
 console.log('🚀  SOCKET_URL:', SOCKET_URL);
 class SocketService {
