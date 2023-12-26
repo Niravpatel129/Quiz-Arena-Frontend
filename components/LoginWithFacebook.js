b4b19ac869626695b8f05c8bfc794a7bbabdafc3
Nirav <niravpatelp129@gmail.com>
2023-12-26 11:57:03 -0500

    Update LoginWithFacebook component styling

--- components/LoginWithFacebook.js
+++ components/LoginWithFacebook.js
@@ -1,7 +1,7 @@
 import * as Facebook from 'expo-auth-session/providers/facebook';
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
-import { Button } from 'react-native';
+import { Pressable, StyleSheet, Text } from 'react-native';
 
 const FB_APP_ID = '1015444866200816';
 
@@ -29,12 +29,32 @@ export default function LoginWithFacebook() {
   }, [response]);
 
   return (
-    <Button
+    <Pressable
+      style={styles.button}
       disabled={!request}
       title='Login with Facebook'
       onPress={() => {
         promptAsync();
       }}
-    />
+    >
+      <Text style={styles.buttonText}>Login with Facebook</Text>
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    margin: 20,
+    width: '80%',
+    padding: 20,
+    borderRadius: 5,
+    backgroundColor: '#457dff',
+    color: '#fff',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+  },
+});
