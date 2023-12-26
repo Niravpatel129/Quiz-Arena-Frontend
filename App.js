c52c8280487f9690336e4b6ec36bbc30b5f09fc6
Nirav <niravpatelp129@gmail.com>
2023-12-26 05:03:16 -0500

    Refactor App component structure

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
-import EnhancedStorybookUIRoot from './EnhancedStorybookUIRoot';
 import { SocketProvider } from './context/socket/SocketContext';
 import CategoriesScreen from './screens/categories';
 import ChallengeScreen from './screens/challenge';
@@ -15,63 +14,59 @@ import QueueScreen from './screens/queue';
 
 const Stack = createNativeStackNavigator();
 
-const SHOW_STORYBOOK = false;
+function App() {
+  return (
+    <NativeBaseProvider>
+      <SocketProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            {/* <Stack.Screen name='Dev' component={Dev} options={{ headerShown: false }} /> */}
+            <Stack.Screen
+              name='Home'
+              component={HomeScreen}
+              options={{ title: 'Welcome', headerShown: false }}
+            />
+            <Stack.Screen
+              name='Categories'
+              component={CategoriesScreen}
+              options={{ title: 'Categories' }}
+            />
+            <Stack.Screen name='Queue' component={QueueScreen} options={{ title: 'Queue' }} />
+            <Stack.Screen
+              name='Game'
+              component={GameScreen}
+              options={{ title: 'Game', headerShown: false }}
+            />
+            <Stack.Screen
+              name='GameOver'
+              component={GameOverScreen}
+              options={{ title: 'Game Over', headerShown: false }}
+            />
+            <Stack.Screen
+              name='Leaderboards'
+              component={LeaderboardsScreen}
+              options={{ title: 'Leaderboards', headerShown: true }}
+            />
+            <Stack.Screen
+              name='Players'
+              component={PlayersScreen}
+              options={{ title: 'Players', headerShown: true }}
+            />
+            <Stack.Screen
+              name='Challenge'
+              component={ChallengeScreen}
+              options={{ title: 'Challenge', headerShown: true }}
+            />
+            <Stack.Screen
+              name='Notifications'
+              component={NotificationsScreen}
+              options={{ title: 'Notifications', headerShown: true }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SocketProvider>
+    </NativeBaseProvider>
+  );
+}
 
-const MainComponent = SHOW_STORYBOOK
-  ? EnhancedStorybookUIRoot
-  : function App() {
-      return (
-        <NativeBaseProvider>
-          <SocketProvider>
-            <NavigationContainer>
-              <Stack.Navigator>
-                {/* <Stack.Screen name='Dev' component={Dev} options={{ headerShown: false }} /> */}
-                <Stack.Screen
-                  name='Home'
-                  component={HomeScreen}
-                  options={{ title: 'Welcome', headerShown: false }}
-                />
-                <Stack.Screen
-                  name='Categories'
-                  component={CategoriesScreen}
-                  options={{ title: 'Categories' }}
-                />
-                <Stack.Screen name='Queue' component={QueueScreen} options={{ title: 'Queue' }} />
-                <Stack.Screen
-                  name='Game'
-                  component={GameScreen}
-                  options={{ title: 'Game', headerShown: false }}
-                />
-                <Stack.Screen
-                  name='GameOver'
-                  component={GameOverScreen}
-                  options={{ title: 'Game Over', headerShown: false }}
-                />
-                <Stack.Screen
-                  name='Leaderboards'
-                  component={LeaderboardsScreen}
-                  options={{ title: 'Leaderboards', headerShown: true }}
-                />
-                <Stack.Screen
-                  name='Players'
-                  component={PlayersScreen}
-                  options={{ title: 'Players', headerShown: true }}
-                />
-                <Stack.Screen
-                  name='Challenge'
-                  component={ChallengeScreen}
-                  options={{ title: 'Challenge', headerShown: true }}
-                />
-                <Stack.Screen
-                  name='Notifications'
-                  component={NotificationsScreen}
-                  options={{ title: 'Notifications', headerShown: true }}
-                />
-              </Stack.Navigator>
-            </NavigationContainer>
-          </SocketProvider>
-        </NativeBaseProvider>
-      );
-    };
-
-export default MainComponent;
+export default App;
