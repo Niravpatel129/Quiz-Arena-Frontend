041174e11d8657c8efcfb156625778087d7a9ab0
Nirav <niravpatelp129@gmail.com>
2023-12-27 08:19:56 -0500

    Update FriendsScreen UI

--- screens/friends/index.js
+++ screens/friends/index.js
@@ -3,8 +3,22 @@ import { Text, View } from 'react-native';
 
 export default function FriendsScreen() {
   return (
-    <View>
-      <Text>Friends</Text>
+    <View
+      style={{
+        backgroundColor: '#1c2141',
+        height: '100%',
+      }}
+    >
+      <Text
+        style={{
+          color: 'white',
+          fontSize: 30,
+          textAlign: 'center',
+          marginTop: 300,
+        }}
+      >
+        Friends W.I.P
+      </Text>
     </View>
   );
 }
