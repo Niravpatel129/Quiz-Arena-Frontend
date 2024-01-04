020ffc91af9665d5ed6a7b0b747777a0f7172c54
Nirav <niravpatelp129@gmail.com>
2024-01-04 08:28:21 -0500

    Add QuestionsPostGame component

---
+++ components/QuestionsPostGame/QuestionsPostGame.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function QuestionsPostGame() {
+  return (
+    <View>
+      <Text>QuestionsPostGame</Text>
+    </View>
+  );
+}
