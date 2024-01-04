f9ec0b0749740d08c613a91c160b3234aaa8c873
Nirav <niravpatelp129@gmail.com>
2024-01-04 16:03:01 -0500

    Add Google services file and enable iOS simulator

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Preview:
 
 ![image](https://github.com/Niravpatel129/Quiz-Arena-Frontend-Scaffold/assets/43049943/8f620589-3471-4e4a-9842-548a94a18029)
+
+<!--       // "googleServicesFile": "./google/elo-mastery-b6f43-f8db4624ebc1.json", -->
---
+++ context/notifications/notificationsContext.js
@@ -0,0 +1,110 @@
+import Constants from 'expo-constants';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import { createContext, useContext, useEffect, useRef, useState } from 'react';
+import { Platform } from 'react-native';
+
+const NotificationsContext = createContext();
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export const NotificationsProvider = ({ children }) => {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  return (
+    <NotificationsContext.Provider value={{}}>
+      {/* <Pressable
+        style={{
+          marginTop: 100,
+          padding: 20,
+          borderRadius: 10,
+          backgroundColor: '#292929',
+        }}
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      >
+        <Text>Press here to schedule a notification</Text>
+      </Pressable> */}
+      {children}
+    </NotificationsContext.Provider>
+  );
+};
+
+export const useNotifications = () => {
+  return useContext(NotificationsContext);
+};
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      console.log('Failed to get push token for push notification!');
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    token = await Notifications.getExpoPushTokenAsync({
+      projectId:
+        Constants.expoConfig?.extra?.eas?.projectId || '8c111cf0-8ea7-4e8d-a8c6-a92bb5e06340',
+    });
+    console.log('🚀  token:', token);
+  } else {
+    console.log('Must use physical device for Push Notifications');
+  }
+
+  return token;
+}
