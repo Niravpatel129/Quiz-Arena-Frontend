1e24125b88a98d3b788564a9146b6f891b2363cf
Nirav <niravpatelp129@gmail.com>
2024-01-06 22:08:53 -0500

    Add image animation to AvatarPicker component

--- components/AvatarPicker/AvatarPicker.js
+++ components/AvatarPicker/AvatarPicker.js
@@ -1,11 +1,27 @@
 import * as ImagePicker from 'expo-image-picker';
 import React, { useState } from 'react';
-import { Alert, Image, TouchableOpacity } from 'react-native';
+import { Alert, Animated, TouchableOpacity } from 'react-native';
 import { newRequest } from '../../api/newRequest';
 import upload from '../../helpers/upload';
 
 const AvatarPicker = ({ defaultImage, disablePress }) => {
   const [avatar, setAvatar] = useState(null);
+  const scaleAnim = new Animated.Value(0.5);
+  const opacityAnim = new Animated.Value(0); // Initial opacity value for fade-in animation
+
+  const onImageLoad = () => {
+    Animated.spring(scaleAnim, {
+      toValue: 1,
+      friction: 5,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(opacityAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  };
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -52,20 +68,33 @@ const AvatarPicker = ({ defaultImage, disablePress }) => {
         borderRadius: 100,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#DDD',
+        // backgroundColor: '#DDD',
         overflow: 'hidden',
       }}
     >
       {avatar ? (
-        <Image source={{ uri: avatar }} style={{ width: 200, height: 200 }} />
-      ) : (
-        <Image
-          source={{
-            uri:
-              defaultImage ||
-              'https://images.unsplash.com/photo-1574144611937-0df059b5ef3e?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnVubnklMjBjYXR8ZW58MHx8MHx8fDA%3D',
+        <Animated.Image
+          source={{ uri: avatar }}
+          style={{
+            width: 200,
+            height: 200,
+            transform: [{ scale: scaleAnim }],
+            opacity: opacityAnim,
           }}
-          style={{ width: 200, height: 200 }}
+          onLoad={onImageLoad}
+        />
+      ) : (
+        <Animated.Image
+          source={{
+            uri: defaultImage,
+          }}
+          style={{
+            width: 200,
+            height: 200,
+            transform: [{ scale: scaleAnim }],
+            opacity: opacityAnim,
+          }}
+          onLoad={onImageLoad}
         />
       )}
     </TouchableOpacity>
