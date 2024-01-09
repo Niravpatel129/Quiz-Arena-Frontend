081c3aa622cd81d8a557c6e244b34fb9e7c2a567
Nirav <niravpatelp129@gmail.com>
2024-01-09 15:16:34 -0500

    Refactor ChallengeScreen component

--- screens/challenge/index.js
+++ screens/challenge/index.js
@@ -1,9 +1,21 @@
-import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { useEffect, useRef, useState } from 'react';
+import { Image, Text, View } from 'react-native';
+import CountryFlag from 'react-native-country-flag';
+import { useAuth } from '../../context/auth/AuthContext';
+import capitalizeFirstLetter from '../../helpers/capitalizeFirstLetter';
 import socketService from '../../services/socketService';
 
 export default function ChallengeScreen({ route, navigation }) {
+  const [queueTime, setQueueTime] = useState(1);
+  const routeParam = route.params;
+  const intervalRef = useRef(null);
+  const categoryName = routeParam?.categoryName || 'Logos';
+  const { userData } = useAuth();
+
   useEffect(() => {
+    startTimer();
+
     socketService.emit('joinChallengeQueue', {
       gameId: route.params.gameId,
       category: route.params.category,
@@ -12,11 +24,161 @@ export default function ChallengeScreen({ route, navigation }) {
     socketService.on('game_start', (data) => {
       navigation.navigate('Game', { game: data.game });
     });
+
+    return () => {
+      socketService.emit('leaveChallengeQueue', {
+        gameId: route.params.gameId,
+        category: route.params.category,
+      });
+    };
   }, []);
 
+  const startTimer = () => {
+    intervalRef.current = setInterval(() => {
+      setQueueTime((prevTime) => prevTime + 1);
+    }, 1000);
+  };
+
+  const PlayerCard = (playerData, isPlaceholder) => {
+    return (
+      <View style={{}}>
+        <Image
+          style={{
+            width: 120,
+            height: 120,
+            borderRadius: 30,
+            overflow: 'hidden',
+            borderWidth: 3,
+            borderColor: '#69829c',
+          }}
+          source={{
+            uri: 'https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg',
+          }}
+        />
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 16,
+            fontFamily: 'Inter-Bold',
+            textAlign: 'center',
+            marginTop: 10,
+            textShadowColor: isPlaceholder ? '#000' : 'transparent',
+            textShadowOffset: { width: 0, height: 0 },
+            textShadowRadius: 10,
+          }}
+        >
+          {isPlaceholder ? 'Searching...' : capitalizeFirstLetter(playerData.playerName)}{' '}
+          <CountryFlag isoCode={playerData.country || ''} size={14} />
+        </Text>
+        <Text
+          style={{
+            color: 'white',
+            fontSize: 16,
+            fontFamily: 'Inter-Bold',
+            textAlign: 'center',
+            marginTop: 2,
+            textShadowColor: isPlaceholder ? '#000' : 'transparent',
+            textShadowOffset: { width: 0, height: 0 },
+            textShadowRadius: 10,
+          }}
+        >
+          {isPlaceholder ? '' : `${playerData.elo} Elo`}
+        </Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>ChallengeScreen {route.params.category}</Text>
-    </View>
+    <LinearGradient
+      colors={['#0f0c29', '#302b63', '#24243e']}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      style={{ height: '100%', padding: 5 }}
+    >
+      <View>
+        <Text>ChallengeScreen {route.params.category}</Text>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            marginTop: 40,
+          }}
+        >
+          <View>
+            {PlayerCard(
+              {
+                tag: 'Player Tag',
+                playerName: userData?.username || 'Alex Smith',
+                country: userData?.country || 'ca',
+                avatar: userData?.avatar || '',
+                elo: userData?.allRating[categoryName] || 1200,
+                experience: userData?.experience,
+              },
+              false,
+            )}
+          </View>
+          <View>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: 45,
+                fontFamily: 'Inter-Regular',
+                textAlign: 'center',
+              }}
+            >
+              VS
+            </Text>
+          </View>
+          <View>{PlayerCard({}, true)}</View>
+        </View>
+        <View
+          style={{
+            marginTop: 40,
+          }}
+        >
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 25,
+              fontFamily: 'Inter-Regular',
+              textAlign: 'center',
+            }}
+          >
+            Waiting for your opponent to join...
+          </Text>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 25,
+              fontFamily: 'Inter-Bold',
+              textAlign: 'center',
+            }}
+          >
+            {capitalizeFirstLetter(categoryName) || 'Logos'}
+          </Text>
+        </View>
+        <View
+          style={{
+            marginTop: 40,
+            alignItems: 'center',
+          }}
+        >
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 16,
+              fontFamily: 'Inter-Regular',
+              textAlign: 'center',
+              marginTop: 10,
+            }}
+          >
+            Time in Queue: {queueTime % 60 < 10 ? '0' + (queueTime % 60) : queueTime % 60}{' '}
+            {queueTime % 60 === 1 ? 'second' : 'seconds'}
+          </Text>
+        </View>
+      </View>
+    </LinearGradient>
   );
 }
