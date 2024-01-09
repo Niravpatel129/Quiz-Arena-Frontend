b01c6936f046d2f64f3636c8086b285fdaf76bb2
Nirav <niravpatelp129@gmail.com>
2024-01-09 18:38:45 -0500

    Refactor capitalizeFirstLetter function

--- helpers/capitalizeFirstLetter.js
+++ helpers/capitalizeFirstLetter.js
@@ -1,8 +1,8 @@
 function capitalizeFirstLetter(string) {
   return string
-    .split(' ') // Split the string into an array of words
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalize the first letter of each word
-    .join(' '); // Join the words back into a single string
+    ?.split(' ')
+    ?.map((word) => word?.charAt(0)?.toUpperCase() + word?.slice(1))
+    ?.join(' ');
 }
 
 export default capitalizeFirstLetter;
