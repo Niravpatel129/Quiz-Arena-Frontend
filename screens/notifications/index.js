95665be348fdf97d9b5a43bd1310fbd03b947710
Nirav <niravpatelp129@gmail.com>
2024-01-12 03:42:15 -0500

    Remove commented out code for fetching notifications

--- screens/notifications/index.js
+++ screens/notifications/index.js
@@ -13,24 +13,9 @@ import { newRequest } from '../../api/newRequest';
 import { useAuth } from '../../context/auth/AuthContext';
 
 export default function NotificationsScreen({ navigation }) {
-  // const [notifications, setNotifications] = React.useState([]);
   const [refreshing, setRefreshing] = React.useState(false);
   const { fetchNotifications, userNotifications } = useAuth();
   const notifications = userNotifications;
-
-  // const fetchNotifications = async () => {
-  //   setRefreshing(true);
-
-  //   try {
-  //     const response = await newRequest.get('/users/notifications');
-  //     const data = response.data;
-  //     setNotifications(data.notifications);
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-
-  //   setRefreshing(false);
-  // };
 
   React.useEffect(() => {
     fetchNotifications();
