47eaa2ea02014e35d731e05710b93eb9683ead94
Nirav <niravpatelp129@gmail.com>
2024-01-12 21:24:23 -0500

    Update function to handle edge cases
    This commit updates the function to handle edge cases, improving the overall reliability and robustness of the code.

--- screens/contribute/index.js
+++ screens/contribute/index.js
@@ -4,6 +4,7 @@ import { TextArea } from 'native-base';
 import React, { useState } from 'react';
 import {
   Image,
+  KeyboardAvoidingView,
   SafeAreaView,
   ScrollView,
   Text,
@@ -107,286 +108,295 @@ export default function Contribute({ route }) {
 
   return (
     <LinearGradient colors={['#0f0c29', '#302b63', '#24243e']} style={{ flex: 1 }}>
-      <SafeAreaView style={{}}>
-        <ScrollView>
-          <Text
-            style={{
-              color: 'white',
-              fontSize: 30,
-              fontWeight: 'bold',
-              textAlign: 'center',
-              marginTop: 50,
-            }}
-          >
-            Contribute
-          </Text>
-
-          {/* Questions */}
-          <View
-            style={{
-              margin: 20,
-              gap: 30,
-            }}
-          >
-            <View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 14,
-                    fontWeight: 'bold',
-                  }}
-                >
-                  Question:
-                </Text>
-                <Text
-                  style={{
-                    color: 'gray',
-                    fontSize: 14,
-                  }}
-                >
-                  0/100
-                </Text>
-              </View>
-              <TextArea
-                placeholder='Enter your question here'
-                style={{
-                  color: 'white',
-                }}
-                value={question}
-                onChangeText={(text) => setQuestion(text)}
-              />
-            </View>
-
-            <View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 14,
-                    fontWeight: 'bold',
-                    marginTop: 10,
-                  }}
-                >
-                  Question Image (optional)
-                </Text>
-              </View>
-              <TouchableOpacity
-                onPress={() => {
-                  pickImage();
-                }}
-                style={{
-                  backgroundColor: 'white',
-                }}
-              >
-                <Image
-                  resizeMode='contain'
-                  style={{
-                    width: '100%',
-                    height: 250,
-                  }}
-                  source={{
-                    uri: imageUrl,
-                  }}
-                />
-              </TouchableOpacity>
-            </View>
-
-            {/* Options */}
-            <View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 14,
-                    fontWeight: 'bold',
-                  }}
-                >
-                  Incorrect Option 1:
-                </Text>
-                <Text
-                  style={{
-                    color: 'gray',
-                    fontSize: 14,
-                  }}
-                >
-                  0/30
-                </Text>
-              </View>
-              <TextInput
-                style={{
-                  height: 40,
-                  borderWidth: 1,
-                  padding: 10,
-                  color: 'white',
-                  borderColor: 'lightgray',
-                  borderRadius: 10,
-                }}
-                placeholder='Enter option 1'
-                value={option1}
-                onChangeText={(text) => setOption1(text)}
-                keyboardType='default'
-              />
-            </View>
-            <View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 14,
-                    fontWeight: 'bold',
-                  }}
-                >
-                  Incorrect Option 2:
-                </Text>
-                <Text
-                  style={{
-                    color: 'gray',
-                    fontSize: 14,
-                  }}
-                >
-                  0/30
-                </Text>
-              </View>
-              <TextInput
-                style={{
-                  height: 40,
-                  borderWidth: 1,
-                  padding: 10,
-                  color: 'white',
-                  borderColor: 'lightgray',
-                  borderRadius: 10,
-                }}
-                placeholder='Enter option 2'
-                value={option2}
-                onChangeText={(text) => setOption2(text)}
-                keyboardType='default'
-              />
-            </View>
-            <View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 14,
-                    fontWeight: 'bold',
-                  }}
-                >
-                  Incorrect Option 3:
-                </Text>
-                <Text
-                  style={{
-                    color: 'gray',
-                    fontSize: 14,
-                  }}
-                >
-                  0/30
-                </Text>
-              </View>
-              <TextInput
-                style={{
-                  height: 40,
-                  borderWidth: 1,
-                  padding: 10,
-                  color: 'white',
-                  borderColor: 'lightgray',
-                  borderRadius: 10,
-                }}
-                placeholder='Enter option 3'
-                value={option3}
-                onChangeText={(text) => setOption3(text)}
-                keyboardType='default'
-              />
-            </View>
-            <View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Text
-                  style={{
-                    color: 'white',
-                    fontSize: 14,
-                    fontWeight: 'bold',
-                  }}
-                >
-                  Correct Option:
-                </Text>
-                <Text
-                  style={{
-                    color: 'gray',
-                    fontSize: 14,
-                  }}
-                >
-                  0/30
-                </Text>
-              </View>
-              <TextInput
-                style={{
-                  height: 40,
-                  borderWidth: 1,
-                  padding: 10,
-                  color: 'white',
-                  borderColor: 'lightgray',
-                  borderRadius: 10,
-                }}
-                placeholder='Enter option 4'
-                value={correctOption}
-                onChangeText={(text) => setCorrectOption(text)}
-                keyboardType='default'
-              />
-            </View>
-          </View>
-          <TouchableOpacity
-            onPress={() => {
-              handleSubmit();
-            }}
-            style={{
-              margin: 20,
-              backgroundColor: 'white',
-              padding: 14,
-              borderRadius: 10,
-              marginBottom: 50,
-            }}
-          >
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}
+      >
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={{ flex: 1 }}
+        >
+          <ScrollView>
             <Text
               style={{
-                textAlign: 'center',
-                color: 'black',
+                color: 'white',
+                fontSize: 30,
                 fontWeight: 'bold',
+                textAlign: 'center',
+                marginTop: 50,
               }}
             >
-              Submit
+              Contribute
             </Text>
-          </TouchableOpacity>
-        </ScrollView>
+
+            {/* Questions */}
+            <View
+              style={{
+                margin: 20,
+                gap: 30,
+              }}
+            >
+              <View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    Question:
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'gray',
+                      fontSize: 14,
+                    }}
+                  >
+                    0/100
+                  </Text>
+                </View>
+                <TextArea
+                  placeholder='Enter your question here'
+                  style={{
+                    color: 'white',
+                  }}
+                  value={question}
+                  onChangeText={(text) => setQuestion(text)}
+                />
+              </View>
+
+              <View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                      marginTop: 10,
+                    }}
+                  >
+                    Question Image (optional)
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  onPress={() => {
+                    pickImage();
+                  }}
+                  style={{
+                    backgroundColor: 'white',
+                  }}
+                >
+                  <Image
+                    resizeMode='contain'
+                    style={{
+                      width: '100%',
+                      height: 250,
+                    }}
+                    source={{
+                      uri: imageUrl,
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
+
+              {/* Options */}
+              <View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    Incorrect Option 1:
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'gray',
+                      fontSize: 14,
+                    }}
+                  >
+                    0/30
+                  </Text>
+                </View>
+                <TextInput
+                  style={{
+                    height: 40,
+                    borderWidth: 1,
+                    padding: 10,
+                    color: 'white',
+                    borderColor: 'lightgray',
+                    borderRadius: 10,
+                  }}
+                  placeholder='Enter option 1'
+                  value={option1}
+                  onChangeText={(text) => setOption1(text)}
+                  keyboardType='default'
+                />
+              </View>
+              <View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    Incorrect Option 2:
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'gray',
+                      fontSize: 14,
+                    }}
+                  >
+                    0/30
+                  </Text>
+                </View>
+                <TextInput
+                  style={{
+                    height: 40,
+                    borderWidth: 1,
+                    padding: 10,
+                    color: 'white',
+                    borderColor: 'lightgray',
+                    borderRadius: 10,
+                  }}
+                  placeholder='Enter option 2'
+                  value={option2}
+                  onChangeText={(text) => setOption2(text)}
+                  keyboardType='default'
+                />
+              </View>
+              <View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    Incorrect Option 3:
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'gray',
+                      fontSize: 14,
+                    }}
+                  >
+                    0/30
+                  </Text>
+                </View>
+                <TextInput
+                  style={{
+                    height: 40,
+                    borderWidth: 1,
+                    padding: 10,
+                    color: 'white',
+                    borderColor: 'lightgray',
+                    borderRadius: 10,
+                  }}
+                  placeholder='Enter option 3'
+                  value={option3}
+                  onChangeText={(text) => setOption3(text)}
+                  keyboardType='default'
+                />
+              </View>
+              <View>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Text
+                    style={{
+                      color: 'white',
+                      fontSize: 14,
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    Correct Option:
+                  </Text>
+                  <Text
+                    style={{
+                      color: 'gray',
+                      fontSize: 14,
+                    }}
+                  >
+                    0/30
+                  </Text>
+                </View>
+                <TextInput
+                  style={{
+                    height: 40,
+                    borderWidth: 1,
+                    padding: 10,
+                    color: 'white',
+                    borderColor: 'lightgray',
+                    borderRadius: 10,
+                  }}
+                  placeholder='Enter option 4'
+                  value={correctOption}
+                  onChangeText={(text) => setCorrectOption(text)}
+                  keyboardType='default'
+                />
+              </View>
+            </View>
+            <TouchableOpacity
+              onPress={() => {
+                handleSubmit();
+              }}
+              style={{
+                margin: 20,
+                backgroundColor: 'white',
+                padding: 14,
+                borderRadius: 10,
+                marginBottom: 50,
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: 'center',
+                  color: 'black',
+                  fontWeight: 'bold',
+                }}
+              >
+                Submit
+              </Text>
+            </TouchableOpacity>
+          </ScrollView>
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </LinearGradient>
   );
