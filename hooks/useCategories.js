8dee28b2da2d1eaa144804d3900b343980e5fc0a
Nirav <niravpatelp129@gmail.com>
2024-02-23 11:58:16 -0500

    Refactor useCategories hook to implement data caching

--- hooks/useCategories.js
+++ hooks/useCategories.js
@@ -2,28 +2,42 @@ import { useEffect, useState } from 'react';
 import { newRequest } from '../api/newRequest';
 import useRecentlyPlayed from './useRecentlyPlayed';
 
+// Module-level cache variables
+let cachedCategories = null;
+let cachedUserData = null;
+let isDataFetched = false;
+
 export default function useCategories() {
-  const [categories, setCategories] = useState([]);
-  const [userData, setUserData] = useState({});
+  const [categories, setCategories] = useState(cachedCategories || []);
+  const [userData, setUserData] = useState(cachedUserData || {});
   const { fetchRecentlyPlayed } = useRecentlyPlayed();
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await newRequest('/homepage/home');
-      const previous = await fetchRecentlyPlayed();
+    if (!isDataFetched) {
+      // Check if data has not been fetched yet
+      const fetchData = async () => {
+        const res = await newRequest('/homepage/home');
+        const previous = await fetchRecentlyPlayed();
 
-      if (previous.length > 0) {
-        res.data.categories.unshift({
-          parentCategory: 'Recently Played',
-          subCategories: previous.map((category) => ({ name: category, image: '' })),
-        });
-      }
+        if (previous.length > 0) {
+          res.data.categories.unshift({
+            parentCategory: 'Recently Played',
+            subCategories: previous.map((category) => ({ name: category, image: '' })),
+          });
+        }
 
-      setCategories(res.data.categories);
-      setUserData(res.data.user);
-    };
+        // Update state with fetched data
+        setCategories(res.data.categories);
+        setUserData(res.data.user);
 
-    fetchData();
+        // Cache the fetched data
+        cachedCategories = res.data.categories;
+        cachedUserData = res.data.user;
+        isDataFetched = true; // Mark as data fetched
+      };
+
+      fetchData();
+    }
   }, []);
 
   return { categories, userData };
