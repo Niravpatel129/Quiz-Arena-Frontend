72b224a1046b54c5632c3880b4d839ed2ccc3fdb
Nirav <niravpatelp129@gmail.com>
2024-02-26 11:56:24 -0500

    Refactor useCategories hook to improve data fetching and update logic

--- hooks/useCategories.js
+++ hooks/useCategories.js
@@ -8,37 +8,53 @@ let cachedUserData = null;
 let isDataFetched = false;
 
 export default function useCategories() {
-  const [categories, setCategories] = useState(cachedCategories || []);
-  const [userData, setUserData] = useState(cachedUserData || {});
+  const [categories, setCategories] = useState([]);
+  const [userData, setUserData] = useState({});
   const { fetchRecentlyPlayed } = useRecentlyPlayed();
 
   useEffect(() => {
-    if (!isDataFetched) {
-      // Check if data has not been fetched yet
-      const fetchData = async () => {
+    const fetchData = async () => {
+      let categoriesData = cachedCategories || [];
+      let userData = cachedUserData || {};
+
+      if (!isDataFetched) {
+        // Fetch categories and user data only if not already fetched
         const res = await newRequest('/homepage/home');
-        const previous = await fetchRecentlyPlayed();
-
-        if (previous.length > 0) {
-          res.data.categories.unshift({
-            parentCategory: 'Recently Played',
-            subCategories: previous.map((category) => ({ name: category, image: '' })),
-          });
-        }
-
-        // Update state with fetched data
-        setCategories(res.data.categories);
-        setUserData(res.data.user);
+        categoriesData = res.data.categories;
+        userData = res.data.user;
 
         // Cache the fetched data
-        cachedCategories = res.data.categories;
-        cachedUserData = res.data.user;
-        isDataFetched = true; // Mark as data fetched
-      };
+        cachedCategories = categoriesData;
+        cachedUserData = userData;
+        isDataFetched = true;
+      }
 
-      fetchData();
-    }
-  }, []);
+      // Always fetch "Recently Played" to keep it updated
+      const previous = await fetchRecentlyPlayed();
+      let updatedCategories = [...categoriesData];
+
+      if (previous.length > 0) {
+        const recentlyPlayedCategory = {
+          parentCategory: 'Recently Played',
+          subCategories: previous.map((category) => ({ name: category, image: '' })),
+        };
+
+        // Inject "Recently Played" at the beginning or update it if it already exists
+        const index = updatedCategories.findIndex((c) => c.parentCategory === 'Recently Played');
+        if (index !== -1) {
+          updatedCategories[index] = recentlyPlayedCategory;
+        } else {
+          updatedCategories.unshift(recentlyPlayedCategory);
+        }
+      }
+
+      // Update state
+      setCategories(updatedCategories);
+      setUserData(userData);
+    };
+
+    fetchData();
+  }, []); // The dependency array is kept empty to emulate componentDidMount behavior
 
   return { categories, userData };
 }
