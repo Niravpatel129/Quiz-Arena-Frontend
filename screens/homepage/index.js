bbedacabf41217a0faf911be62328eabb89a5831
Nirav <niravpatelp129@gmail.com>
2024-03-07 04:26:30 -0500

    Add update notification and link to app store/play store

--- screens/homepage/index.js
+++ screens/homepage/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView } from 'react-native';
+import { Linking, Platform, ScrollView } from 'react-native';
 import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
+import Toast from 'react-native-toast-message';
 import { newRequest } from '../../api/newRequest';
 import useCategories from '../../hooks/useCategories';
 import useStreak from '../../hooks/useStreak';
@@ -34,6 +35,26 @@ export default function Homepage() {
       const res = await newRequest.get(`/homepage/config/${keys.version}`);
 
       setConfig(res.data);
+      console.log('🚀  res:', res.data);
+
+      if (true || res.data?.updatedRequired) {
+        Toast.show({
+          type: 'info',
+          position: 'bottom',
+          text1: 'Update Available',
+          text2: 'A new version of the app is available, we recommend updating it now.',
+          visibilityTime: 3000,
+          autoHide: false,
+          onPress: () => {
+            const link =
+              Platform.OS === 'ios'
+                ? 'https://apps.apple.com/ca/app/quiz-arena-trivia-questions/id6474947179'
+                : 'https://play.google.com/store/apps/details?id=com.niravpatelp129.QuizArenaFrontendScaffold';
+
+            Linking.openURL(link);
+          },
+        });
+      }
     };
 
     fetchConfig();
