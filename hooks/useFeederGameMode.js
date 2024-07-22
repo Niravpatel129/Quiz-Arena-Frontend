88b1a33427c7360c937595c8fbbcf0248d998296
Ashish Patel <apatel2@ualberta.ca>
2024-07-22 14:56:18 -0600

    Add state variables to track bonus option usage in useFeederGameMode

--- hooks/useFeederGameMode.js
+++ hooks/useFeederGameMode.js
@@ -1,23 +1,20 @@
-import { useCallback, useEffect, useState } from 'react';
-import Toast from 'react-native-toast-message';
-import { newRequest } from '../api/newRequest';
+import { useCallback, useEffect, useState } from "react";
+import Toast from "react-native-toast-message";
+import { newRequest } from "../api/newRequest";
 
-// Function to fetch questions from the server
 const fetchQuestions = async (numQuestions, startOrder = 0, category) => {
   try {
     const response = await newRequest.get(
-      `/feeder/questions?numQuestions=${numQuestions}&startOrder=${startOrder}&category=${category}`,
+      `/feeder/questions?numQuestions=${numQuestions}&startOrder=${startOrder}&category=${category}`
     );
     return response.data;
   } catch (error) {
-    console.error('Error fetching questions:', error);
+    console.error("Error fetching questions:", error);
     return [];
   }
 };
 
-// Custom hook to manage the feeder game mode
-const useFeederGameMode = (category = 'logos') => {
-  // State variables
+const useFeederGameMode = (category = "logos") => {
   const [questions, setQuestions] = useState([]);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [gameActive, setGameActive] = useState(false);
@@ -28,14 +25,18 @@ const useFeederGameMode = (category = 'logos') => {
   const [results, setResults] = useState([]);
   const [gameOver, setGameOver] = useState(false);
 
-  // useEffect to fetch questions when game starts
+  // State variables to track bonus options
+  const [fiftyFiftyCount, setFiftyFiftyCount] = useState(2);
+  const [redoCount, setRedoCount] = useState(2);
+  const [bonusTimeCount, setBonusTimeCount] = useState(3);
+  const [hintCount, setHintCount] = useState(1);
+
   useEffect(() => {
     if (gameActive) {
       fetchQuestions(10, 0, category).then((qs) => setQuestions(qs));
     }
   }, [gameActive]);
 
-  // Function to start the game
   const startGame = () => {
     setGameActive(true);
     setCurrentQuestionIndex(0);
@@ -43,23 +44,26 @@ const useFeederGameMode = (category = 'logos') => {
     setShowPickPercentage(false);
     setWaitingForNext(false);
     setScore(0);
+    setFiftyFiftyCount(2); // Reset counts when the game starts
+    setRedoCount(2);
+    setBonusTimeCount(3);
+    setHintCount(1);
   };
 
-  // Function to submit user answers to the server
   const submitUserAnswers = async () => {
     try {
       if (userAnswers.length === 0) return;
-      await newRequest.post('/feeder/user-answers', userAnswers);
-      console.log('User answers submitted successfully');
+      await newRequest.post("/feeder/user-answers", userAnswers);
+      console.log("User answers submitted successfully");
     } catch (error) {
-      console.error('Error submitting user answers:', error);
+      console.error("Error submitting user answers:", error);
     }
   };
 
-  // Function to handle answering a question
   const answerQuestion = useCallback(
     (answer) => {
-      const isCorrect = questions[currentQuestionIndex].correctAnswer === answer;
+      const isCorrect =
+        questions[currentQuestionIndex].correctAnswer === answer;
 
       setUserAnswers((prevUserAnswers) => [
         ...prevUserAnswers,
@@ -77,27 +81,26 @@ const useFeederGameMode = (category = 'logos') => {
         setScore((prevScore) => prevScore + 1);
       }
     },
-    [questions, currentQuestionIndex],
+    [questions, currentQuestionIndex]
   );
 
-  // Function to upload feeder results to the server
   const uploadFeederResults = async () => {
     try {
-      const res = await newRequest.post('/feeder/results', {
+      const res = await newRequest.post("/feeder/results", {
         scoreAchieved: score,
         category: category,
       });
       setResults(res.data);
-      console.log('Feeder results uploaded successfully');
+      console.log("Feeder results uploaded successfully");
     } catch (error) {
-      console.error('Error uploading feeder results:', error);
+      console.error("Error uploading feeder results:", error);
     }
   };
 
-  // Function to continue the game
   const continueGame = useCallback(() => {
     const isCorrect =
-      questions[currentQuestionIndex]?.correctAnswer === userAnswers[currentQuestionIndex]?.answer;
+      questions[currentQuestionIndex]?.correctAnswer ===
+      userAnswers[currentQuestionIndex]?.answer;
     if (!isCorrect && gameActive) {
       setGameActive(false);
       setGameOver(true); // Mark the game as over
@@ -108,34 +111,37 @@ const useFeederGameMode = (category = 'logos') => {
       if (nextIndex < questions.length) {
         setCurrentQuestionIndex(nextIndex);
       } else {
-        fetchQuestions(5, questions[questions.length - 1].order + 1, category).then(
-          (newQuestions) => {
-            setQuestions((prevQuestions) => [...prevQuestions, ...newQuestions]);
-            if (newQuestions.length === 0) {
-              // Check if no new questions were fetched
-              Toast.show({
-                type: 'error',
-                text1: 'No more questions',
-                text2: 'You have reached the end of the game',
-              });
+        fetchQuestions(
+          5,
+          questions[questions.length - 1].order + 1,
+          category
+        ).then((newQuestions) => {
+          setQuestions((prevQuestions) => [...prevQuestions, ...newQuestions]);
+          if (newQuestions.length === 0) {
+            Toast.show({
+              type: "error",
+              text1: "No more questions",
+              text2: "You have reached the end of the game",
+            });
 
-              setGameOver(true);
-              setGameActive(false); // End the game if there are no more questions
-              submitUserAnswers();
-              uploadFeederResults();
-            } else {
-              setQuestions((prevQuestions) => [...prevQuestions, ...newQuestions]);
-              setCurrentQuestionIndex(nextIndex);
-            }
-          },
-        );
+            setGameOver(true);
+            setGameActive(false); // End the game if there are no more questions
+            submitUserAnswers();
+            uploadFeederResults();
+          } else {
+            setQuestions((prevQuestions) => [
+              ...prevQuestions,
+              ...newQuestions,
+            ]);
+            setCurrentQuestionIndex(nextIndex);
+          }
+        });
       }
     }
     setShowPickPercentage(false);
     setWaitingForNext(false);
   }, [currentQuestionIndex, questions, userAnswers, gameActive]);
 
-  // Return the state and functions to be used in the component
   return {
     questions,
     currentQuestionIndex,
@@ -149,6 +155,14 @@ const useFeederGameMode = (category = 'logos') => {
     results,
     gameOver,
     setGameOver,
+    fiftyFiftyCount,
+    redoCount,
+    bonusTimeCount,
+    hintCount,
+    setFiftyFiftyCount,
+    setRedoCount,
+    setBonusTimeCount,
+    setHintCount,
   };
 };
 
