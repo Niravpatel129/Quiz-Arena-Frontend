748aa8ba88feab5355d682df4a46229af1183a44
Ashish Patel <apatel2@ualberta.ca>
2024-07-31 21:08:11 -0600

    added win or lose avatars with animation as well as a layered effect animation with particles ontop, also minor styling changes

--- screens/gameover3/components/TryAgain.js
+++ screens/gameover3/components/TryAgain.js
@@ -1,74 +1,151 @@
-import React, { useEffect, useRef } from 'react';
-import { Animated, View } from 'react-native';
+import React, { useEffect } from "react";
+import { View } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  withRepeat,
+  withTiming,
+  withSequence,
+} from "react-native-reanimated";
 
 export default function TryAgain({ didWin }) {
-  const opacityAnim = useRef(new Animated.Value(0)).current;
-  const scaleAnim = useRef(new Animated.Value(0.5)).current;
-  const rotateAnim = useRef(new Animated.Value(0)).current; // Use for spring rotation
+  const opacityAnim = useSharedValue(0);
+  const scaleAnim = useSharedValue(0.5);
+  const rotateAnim = useSharedValue(0);
+  const idleAnim = useSharedValue(1);
+  const particlesOpacityAnim = useSharedValue(0);
+  const particlesScaleAnim = useSharedValue(1);
 
   useEffect(() => {
-    Animated.sequence([
-      Animated.delay(500),
-      Animated.parallel([
-        Animated.timing(opacityAnim, {
-          toValue: 1,
-          duration: 500,
-          useNativeDriver: true,
-        }),
-        Animated.spring(scaleAnim, {
-          toValue: 1,
-          friction: 4,
-          useNativeDriver: true,
-        }),
-        // Spring animation for rotation
-        Animated.spring(rotateAnim, {
-          toValue: 1, // End value for the rotation
-          friction: 5, // Customize the bounciness
-          useNativeDriver: true,
-        }),
-      ]),
-    ]).start();
+    // Load-in animation
+    opacityAnim.value = withTiming(1, { duration: 500 });
+    scaleAnim.value = withSequence(
+      withSpring(1.2, { friction: 3 }),
+      withSpring(1, { friction: 4 })
+    );
+    rotateAnim.value = withSpring(1, { friction: 5 });
+
+    // Idle animation
+    idleAnim.value = withRepeat(
+      withSequence(
+        withTiming(1.05, { duration: 2000 }),
+        withTiming(1, { duration: 2000 })
+      ),
+      -1,
+      true
+    );
+
+    // Particles animation
+    particlesOpacityAnim.value = withRepeat(
+      withSequence(
+        withTiming(1, { duration: 2000 }),
+        withTiming(0, { duration: 2000 })
+      ),
+      -1,
+      true
+    );
+    particlesScaleAnim.value = withRepeat(
+      withSequence(
+        withTiming(1.1, { duration: 2000 }),
+        withTiming(1, { duration: 2000 })
+      ),
+      -1,
+      true
+    );
   }, []);
 
-  // Interpolate rotate value for a 0 to 360 degrees rotation
-  const rotate = rotateAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '360deg'],
+  const animatedImageStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacityAnim.value,
+      transform: [
+        { scale: scaleAnim.value },
+        { rotate: `${rotateAnim.value * 360}deg` },
+        { scale: idleAnim.value },
+      ],
+    };
   });
+
+  const animatedTextStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacityAnim.value,
+      transform: [{ scale: scaleAnim.value }],
+    };
+  });
+
+  const animatedParticlesStyle = useAnimatedStyle(() => {
+    return {
+      opacity: particlesOpacityAnim.value,
+      transform: [{ scale: particlesScaleAnim.value }],
+    };
+  });
+
+  const winImage =
+    "https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Faxoltol-you-won.png?alt=media&token=c79e7629-3625-4541-bd1f-2472ef258be4";
+  const loseImage =
+    "https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/axolotl-profile-avatars%2Faxolotl-you-lost.png?alt=media&token=6777d007-9afa-4046-8aba-013cdc673910";
 
   return (
     <View
       style={{
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
       }}
     >
       <View>
         <Animated.Image
-          source={require('../../../assets/smiley_face.png')}
-          style={{
-            width: 100,
-            height: 100,
-            marginBottom: 10,
-            transform: [
-              // Apply spring rotation here
-              { rotate },
-              // Apply additional transform if needed, for example, the win/lose flip
-              { rotate: didWin ? '0deg' : '180deg' },
-            ],
-          }}
+          source={{ uri: didWin ? winImage : loseImage }}
+          style={[
+            {
+              width: 100,
+              height: 100,
+            },
+            animatedImageStyle,
+          ]}
         />
+        {didWin && (
+          <Animated.Image
+            source={require("../../../assets/pink-particles.png")}
+            style={[
+              {
+                position: "absolute",
+                width: 120,
+                height: 120,
+                top: -10,
+                left: -10,
+              },
+              animatedParticlesStyle,
+            ]}
+          />
+        )}
+        {!didWin && (
+          <Animated.Image
+            source={require("../../../assets/tear-particles2.png")}
+            style={[
+              {
+                position: "absolute",
+                width: 80,
+                height: 80,
+                top: 40,
+                left: 0,
+              },
+              animatedParticlesStyle,
+            ]}
+          />
+        )}
       </View>
       <Animated.Text
-        style={{
-          fontFamily: 'poppins-semiBold',
-          fontSize: 26,
-          color: '#181A17',
-          opacity: opacityAnim,
-          transform: [{ scale: scaleAnim }],
-        }}
+        style={[
+          {
+            fontFamily: "poppins-semiBold",
+            fontSize: 26,
+            color: "#181A17",
+          },
+          animatedTextStyle,
+        ]}
       >
-        {didWin ? 'Yay! You won! 🎉' : 'Oops! You lost 🚨'}
+        {didWin ? "Yay! You won! 🎉" : "Oops! You lost 🚨"}
       </Animated.Text>
     </View>
   );
