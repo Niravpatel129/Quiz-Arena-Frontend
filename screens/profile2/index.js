53a48ce946e2d246466cee02545d31f58214a69f
Ashish Patel <apatel2@ualberta.ca>
2024-08-02 20:07:54 -0600

    changed styling, replaced old logic to save avatar and username, now  both are saved through different fields, added bottom sheet with different stlying and components, now  allows users to set avatars while having a different snap point to change background

--- screens/profile2/index.js
+++ screens/profile2/index.js
@@ -1,50 +1,63 @@
-import { Ionicons } from '@expo/vector-icons';
-import { BottomSheetBackdrop, BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
-import { useNavigation } from '@react-navigation/native';
-import { Image } from 'expo-image';
-import { LinearGradient } from 'expo-linear-gradient';
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Ionicons } from "@expo/vector-icons";
 import {
-  Button,
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { useNavigation } from "@react-navigation/native";
+import { Image } from "expo-image";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import {
   SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
-} from 'react-native';
-import CountryFlag from 'react-native-country-flag';
+  Modal,
+} from "react-native";
+import CountryFlag from "react-native-country-flag";
 import Animated, {
   Easing,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
-} from 'react-native-reanimated';
-import { RFValue } from 'react-native-responsive-fontsize';
-import Toast from 'react-native-toast-message';
-import { newRequest } from '../../api/newRequest';
-import formatLastActive from '../../helpers/formatLastActive';
-import CombinedModal from './components/CombinedModal';
+} from "react-native-reanimated";
+import { RFValue } from "react-native-responsive-fontsize";
+import Toast from "react-native-toast-message";
+import { newRequest } from "../../api/newRequest";
+import formatLastActive from "../../helpers/formatLastActive";
+import AvatarBottomSheet from "./components/CombinedModal/AvatarBottomSheet";
+import { LinearGradient } from "expo-linear-gradient";
+import UsernameInput from "./components/CombinedModal/UsernameInput";
 
 export default function Profile2({ userId }) {
   const [userData, setUserData] = useState(null);
   const navigation = useNavigation();
   const opacity = useSharedValue(0);
   const translateY = useSharedValue(30);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [newUsername, setNewUsername] = useState(userData?.username);
-  const [selectedAvatar, setSelectedAvatar] = useState(userData?.avatar);
+  const [defaultUsername, setDefaultUsername] = useState("");
+  const [newUsername, setNewUsername] = useState("");
+  const [selectedAvatar, setSelectedAvatar] = useState("");
+  const [avatarBackground, setAvatarBackground] = useState("#ffffff");
+  const [isUsernameModalVisible, setIsUsernameModalVisible] = useState(false);
 
   // Bottom Sheet
   const bottomSheetModalRef = useRef(null);
-  const snapPoints = useMemo(() => ['25%', '50%'], []);
+  const initialSnapPoints = useMemo(() => ["50%"], []);
 
   const handlePresentModalPress = useCallback(() => {
     bottomSheetModalRef.current?.present();
   }, []);
 
   const handleSheetChanges = useCallback((index) => {
-    console.log('handleSheetChanges', index);
+    console.log("handleSheetChanges", index);
   }, []);
 
   const renderBackdrop = useCallback(
@@ -54,10 +67,10 @@ export default function Profile2({ userId }) {
         disappearsOnIndex={-1}
         appearsOnIndex={0}
         opacity={0.5}
-        pressBehavior='close'
+        pressBehavior="close"
       />
     ),
-    [],
+    []
   );
 
   const animatedStyle = useAnimatedStyle(() => {
@@ -79,79 +92,55 @@ export default function Profile2({ userId }) {
     const fetchUser = async () => {
       const userRes = await newRequest.get(`/users/${userId}`);
       setUserData(userRes.data);
+      setNewUsername(userRes.data.username);
+      setDefaultUsername(userRes.data.username);
     };
 
     fetchUser();
   }, []);
 
-  const handleSave = (avatar, username) => {
+  const handleSave = (avatar, background) => {
     setSelectedAvatar(avatar);
-    setNewUsername(username);
-    setModalVisible(false);
-    // Update user avatar and username on the server
+    setAvatarBackground(background);
     newRequest
-      .put(`/users/avatar`, { avatar, username })
+      .put(`/users/avatar`, { avatar, background })
       .then(() => {
         Toast.show({
-          type: 'success',
-          text1: 'Success',
-          text2: 'Profile updated successfully',
+          type: "success",
+          text1: "Success",
+          text2: "Profile updated successfully",
         });
       })
       .catch((e) => {
         Toast.show({
-          type: 'error',
-          text1: 'Error',
-          text2: 'An error occurred while updating your profile',
+          type: "error",
+          text1: "Error",
+          text2: "An error occurred while updating your profile",
         });
         console.log(e);
       });
   };
 
-  const renderTrophyCard = ({ title, image }) => {
-    console.log('🚀  image:', image);
-    if (image.includes('cloudinary') || image.includes('discordapp')) {
-      image = 'https://cdn-icons-png.flaticon.com/512/476/476851.png';
-    }
-
-    return (
-      <View
-        key={title}
-        style={[
-          {
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '30%',
-          },
-        ]}
-      >
-        <Image
-          cachePolicy='memory-disk'
-          contentFit='contain'
-          source={{
-            uri:
-              image ||
-              'https://res.cloudinary.com/dwu4qop1o/image/upload/v1708638053/Style1_1_gjosgx.png',
-          }}
-          style={{
-            width: 100,
-            height: 100,
-          }}
-        />
-        <Text
-          style={{
-            marginTop: 10,
-            fontFamily: 'poppins-bold',
-            fontSize: 16,
-            color: '#181A17',
-            textAlign: 'center',
-            textTransform: 'capitalize',
-          }}
-        >
-          {title}
-        </Text>
-      </View>
-    );
+  const handleUsernameChange = () => {
+    newRequest
+      .put(`/users`, { username: newUsername })
+      .then(() => {
+        setUserData({ ...userData, username: newUsername });
+        setIsUsernameModalVisible(false);
+        Toast.show({
+          type: "success",
+          text1: "Success",
+          text2: "Username updated successfully",
+        });
+      })
+      .catch((e) => {
+        Toast.show({
+          type: "error",
+          text1: "Error",
+          text2: "Sorry that username is already taken!",
+        });
+        console.log(e);
+      });
   };
 
   const renderStatsCard = (title, value, variation) => {
@@ -160,21 +149,21 @@ export default function Profile2({ userId }) {
     let shadowColor;
 
     if (variation === 1) {
-      colors = ['#CCB6FF', '#9769FF'];
-      shadowColor = 'rgba(169, 131, 255, 0.50)';
-      icon = 'ios-star';
+      colors = ["#CCB6FF", "#9769FF"];
+      shadowColor = "rgba(169, 131, 255, 0.50)";
+      icon = "ios-star";
     }
 
     if (variation === 2) {
-      colors = ['#FFD77F', '#FF9F43'];
-      shadowColor = 'rgba(255, 159, 67, 0.50)';
-      icon = 'ios-bonfire';
+      colors = ["#FFD77F", "#FF9F43"];
+      shadowColor = "rgba(255, 159, 67, 0.50)";
+      icon = "ios-bonfire";
     }
 
     if (variation === 3) {
-      colors = ['#1BEBB9', '#1A9B65'];
-      shadowColor = 'rgba(27, 235, 185, 0.50)';
-      icon = 'ios-trophy';
+      colors = ["#1BEBB9", "#1A9B65"];
+      shadowColor = "rgba(27, 235, 185, 0.50)";
+      icon = "ios-trophy";
     }
 
     return (
@@ -193,17 +182,17 @@ export default function Profile2({ userId }) {
       >
         <View
           style={{
-            alignItems: 'center',
-            justifyContent: 'space-between',
+            alignItems: "center",
+            justifyContent: "space-between",
           }}
         >
-          <Ionicons name={icon} size={24} color='#fff' />
+          <Ionicons name={icon} size={24} color="#fff" />
           <Text
             style={{
-              color: '#fff',
-              fontFamily: 'poppins-regular',
+              color: "#fff",
+              fontFamily: "poppins-regular",
               fontWeight: 400,
-              textAlign: 'center',
+              textAlign: "center",
               fontSize: RFValue(13),
             }}
           >
@@ -211,8 +200,8 @@ export default function Profile2({ userId }) {
           </Text>
           <Text
             style={{
-              color: '#fff',
-              fontFamily: 'poppins-bold',
+              color: "#fff",
+              fontFamily: "poppins-bold",
               fontWeight: 600,
               fontSize: 24,
             }}
@@ -227,144 +216,93 @@ export default function Profile2({ userId }) {
   if (!userData) {
     return null;
   }
-  if (userId && !userData)
-    return (
-      <View
-        style={{
-          height: '100%',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: 'white',
-        }}
-      >
-        <Text
-          style={{
-            opacity: 0.5,
-          }}
-        >
-          Loading...
-        </Text>
-      </View>
-    );
 
   return (
-    <View
-      style={{
-        height: '100%',
-        backgroundColor: 'white',
-      }}
-    >
-      <CombinedModal
-        visible={modalVisible}
-        onClose={() => setModalVisible(false)}
-        onSave={handleSave}
-        defaultUsername={newUsername}
-        defaultAvatar={selectedAvatar}
-      />
-
+    <View style={{ height: "100%", backgroundColor: "white" }}>
       <SafeAreaView>
         {userId && (
           <TouchableOpacity
             onPress={() => navigation.goBack()}
-            style={{
-              padding: 20,
-            }}
+            style={{ padding: 20 }}
           >
-            <Ionicons name='ios-arrow-back' size={24} color='#262625' />
+            <Ionicons name="ios-arrow-back" size={24} color="#262625" />
           </TouchableOpacity>
         )}
 
         <ScrollView
           showsVerticalScrollIndicator={false}
-          style={{
-            marginHorizontal: 10,
-          }}
+          style={{ marginHorizontal: 10 }}
         >
-          <TouchableOpacity
-            onPress={() => {
-              if (userId) return;
-              setModalVisible(true);
-            }}
-          >
+          <TouchableOpacity onPress={handlePresentModalPress}>
             <Animated.View
               style={[
                 animatedStyle,
-                {
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                },
+                { alignItems: "center", justifyContent: "center" },
               ]}
             >
-              <Image
-                cachePolicy='memory-disk'
-                style={{
-                  width: 140,
-                  height: 140,
-                  borderRadius: 150,
-                }}
-                source={{
-                  uri:
-                    selectedAvatar ||
-                    userData?.avatar ||
-                    'https://thumbs.dreamstime.com/b/astronaut-cat-wearing-space-suit-elements-image-furnished-nasa-first-trip-to-space-mixed-media-167670791.jpg',
-                }}
-              ></Image>
+              <View
+                style={{ backgroundColor: avatarBackground, borderRadius: 75 }}
+              >
+                <Image
+                  cachePolicy="memory-disk"
+                  style={{ width: 140, height: 140, borderRadius: 75 }}
+                  source={{
+                    uri:
+                      selectedAvatar ||
+                      userData?.avatar ||
+                      "https://thumbs.dreamstime.com/b/astronaut-cat-wearing-space-suit-elements-image-furnished-nasa-first-trip-to-space-mixed-media-167670791.jpg",
+                  }}
+                />
+              </View>
             </Animated.View>
           </TouchableOpacity>
 
           <View
             style={{
-              textAlign: 'center',
-              alignItems: 'center',
-              justifyContent: 'center',
-              flexDirection: 'row',
+              textAlign: "center",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "row",
               gap: 5,
               marginTop: 10,
             }}
           >
-            <TouchableOpacity
-              onPress={() => {
-                if (userId) return;
-                setModalVisible(true);
-              }}
-            >
+            <TouchableOpacity onPress={() => setIsUsernameModalVisible(true)}>
               <Text
                 style={{
-                  color: '#262625',
+                  color: "#262625",
                   fontSize: 24,
-                  fontFamily: 'poppins-regular',
+                  fontFamily: "poppins-regular",
                 }}
               >
                 {newUsername || userData?.username}
               </Text>
             </TouchableOpacity>
-            {userData?.country && <CountryFlag isoCode={userData?.country} size={20} />}
+            {userData?.country && (
+              <CountryFlag isoCode={userData?.country} size={20} />
+            )}
           </View>
           <Text
             style={{
-              color: '#5E6064',
+              color: "#5E6064",
               fontSize: 13,
-              fontFamily: 'poppins-regular',
-              textAlign: 'center',
+              fontFamily: "poppins-regular",
+              textAlign: "center",
               marginBottom: 10,
             }}
           >
-            {/* Only show this if its last 5 days */}
-            {new Date().getTime() - new Date(userData?.lastActive).getTime() < 432000000 ? (
+            {/* Only show this if it's last 5 days */}
+            {new Date().getTime() - new Date(userData?.lastActive).getTime() <
+            432000000 ? (
               <>Last Active {formatLastActive(userData?.lastActive)}</>
             ) : null}
           </Text>
-          <View
-            style={{
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
+          <View style={{ alignItems: "center", justifyContent: "center" }}>
             <Text
               style={{
-                color: '#FF4646',
+                color: "#FF4646",
                 fontSize: 14,
-                fontFamily: 'poppins-regular',
+                fontFamily: "poppins-regular",
               }}
             >
               Rookie | {userData?.experience} XP
@@ -372,16 +310,16 @@ export default function Profile2({ userId }) {
           </View>
           <View
             style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
             <View
               style={{
-                backgroundColor: '#EFF8FF',
-                flexDirection: 'row',
-                alignItems: 'center',
+                backgroundColor: "#EFF8FF",
+                flexDirection: "row",
+                alignItems: "center",
                 paddingVertical: 12,
                 borderRadius: 100,
                 marginTop: 10,
@@ -389,11 +327,11 @@ export default function Profile2({ userId }) {
                 gap: 5,
               }}
             >
-              <Ionicons name='ios-star' size={24} color='#FDD92C' />
+              <Ionicons name="ios-star" size={24} color="#FDD92C" />
               <Text
                 style={{
-                  color: '#3F95F2',
-                  fontFamily: 'poppins-bold',
+                  color: "#3F95F2",
+                  fontFamily: "poppins-bold",
                   fontWeight: 600,
                 }}
               >
@@ -401,40 +339,65 @@ export default function Profile2({ userId }) {
               </Text>
             </View>
           </View>
-          {/* Cards */}
           <Animated.View
             style={[
               animatedStyle,
-              {
-                flexDirection: 'row',
-                gap: 5,
-                marginTop: 20,
-              },
+              { flexDirection: "row", gap: 5, marginTop: 20 },
             ]}
           >
-            {renderStatsCard('Games', userData?.totalGames || 0, 1)}
-            {renderStatsCard('Win Rate', `${Math.floor(userData?.winRate || null)}%`, 2)}
-            {renderStatsCard('Avg Score', 85, 3)}
+            {renderStatsCard("Games", userData?.totalGames || 0, 1)}
+            {renderStatsCard(
+              "Win Rate",
+              `${Math.floor(userData?.winRate || 0)}%`,
+              2
+            )}
+            {renderStatsCard("Avg Score", 85, 3)}
           </Animated.View>
-
-          {/* Bottom Sheet Button */}
-          <Button title='Open Bottom Sheet' onPress={handlePresentModalPress} />
         </ScrollView>
       </SafeAreaView>
 
-      {/* Bottom Sheet */}
-      <BottomSheetModal
+      <AvatarBottomSheet
         ref={bottomSheetModalRef}
-        index={1}
-        snapPoints={snapPoints}
+        snapPoints={initialSnapPoints}
         onChange={handleSheetChanges}
-        style={styles.bottomSheet}
-        backdropComponent={renderBackdrop}
+        renderBackdrop={renderBackdrop}
+        selectedAvatar={selectedAvatar}
+        setSelectedAvatar={setSelectedAvatar}
+        onSaveChanges={handleSave}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isUsernameModalVisible}
+        onRequestClose={() => setIsUsernameModalVisible(false)}
       >
-        <BottomSheetView style={styles.contentContainer}>
-          <Text>Awesome 🎉</Text>
-        </BottomSheetView>
-      </BottomSheetModal>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <UsernameInput
+              defaultUsername={defaultUsername}
+              newUsername={newUsername}
+              setNewUsername={setNewUsername}
+              usernameInputVisible={true}
+              error=""
+            />
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={[styles.button, styles.buttonCancel]}
+                onPress={() => setIsUsernameModalVisible(false)}
+              >
+                <Text style={styles.textStyle}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[styles.button, styles.buttonSave]}
+                onPress={handleUsernameChange}
+              >
+                <Text style={styles.textStyle}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -442,9 +405,51 @@ export default function Profile2({ userId }) {
 const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   bottomSheet: {
     zIndex: 1000,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 15,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    marginHorizontal: 10,
+  },
+  buttonCancel: {
+    backgroundColor: "#2196F3",
+  },
+  buttonSave: {
+    backgroundColor: "#4CAF50",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
