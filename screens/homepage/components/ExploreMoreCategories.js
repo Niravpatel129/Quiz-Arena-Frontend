cf10734153a16a20f6ead39c1cc56808129d9c59
Ashish Patel <apatel2@ualberta.ca>
2024-08-13 11:15:09 -0600

    exploremorecategories tile with routing to secondary homepage

---
+++ screens/homepage/components/ExploreMoreCategories.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { TouchableOpacity, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+export default function ExploreMoreCategories() {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate("SecondaryHomepage");
+      }}
+      style={{
+        padding: 20,
+        backgroundColor: "#3F95F2",
+        borderRadius: 16,
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ color: "#fff", fontSize: 18, fontWeight: "bold" }}>
+        Explore More Categories
+      </Text>
+    </TouchableOpacity>
+  );
+}
