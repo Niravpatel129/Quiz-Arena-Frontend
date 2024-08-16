5a616d385dda4f4e9f7c5c322cecef11ee64409e
Ashish Patel <apatel2@ualberta.ca>
2024-08-15 18:02:02 -0600

    readded user profile and update styling

--- screens/homepage/components/UserProfile.js
+++ screens/homepage/components/UserProfile.js
@@ -1,6 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import CountryFlag from "react-native-country-flag";
@@ -14,9 +13,7 @@ export default function UserProfile({ userData }) {
 
   return (
     <View>
-      <DividerHeader headerText={"Your Profile"} />
-      <LinearGradient
-        colors={["#EC80B4", "#3F95F2"]}
+      <View
         style={{
           borderRadius: 16,
           shadowColor: "#3E75D9",
@@ -27,46 +24,121 @@ export default function UserProfile({ userData }) {
           shadowOpacity: 0.4,
           shadowRadius: 8,
           elevation: 5,
-          paddingVertical: 20,
-          paddingHorizontal: 10,
           flexDirection: "row",
           alignItems: "center",
+          justifyContent: "space-between",
         }}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
       >
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate("Profile");
-          }}
-          style={{
-            position: "relative",
-            overflow: "hidden",
-          }}
-        >
-          <Image
-            cachePolicy="memory-disk"
-            source={{
-              uri:
-                userData?.profile?.avatar ||
-                "https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/home_page_imgs%2Faxoltol-profile-pic.jpg?alt=media&token=1e49a0a3-9d7e-4d0c-bb75-a401c3f14825",
+        {/* Avatar, Username, Country Flag, Experience, Coins */}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Profile");
             }}
             style={{
-              width: 100,
-              height: 100,
-              borderRadius: 25,
-              borderWidth: 1,
-              borderColor: "#F8D2E6",
-              marginRight: 13,
+              position: "relative",
+              overflow: "hidden",
             }}
-          ></Image>
-        </TouchableOpacity>
+          >
+            <Image
+              cachePolicy="memory-disk"
+              source={{
+                uri:
+                  userData?.profile?.avatar ||
+                  "https://firebasestorage.googleapis.com/v0/b/quiz-arena-e2415.appspot.com/o/home_page_imgs%2Faxoltol-profile-pic.jpg?alt=media&token=1e49a0a3-9d7e-4d0c-bb75-a401c3f14825",
+              }}
+              style={{
+                width: 100,
+                height: 100,
+                borderRadius: 25,
+                borderWidth: 1,
+                borderColor: "#F8D2E6",
+                marginRight: 10,
+              }}
+            />
+          </TouchableOpacity>
 
-        <View
-          style={{
-            alignItems: "flex-start",
-          }}
-        >
+          <View style={{ alignItems: "flex-start" }}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Text
+                style={{
+                  fontFamily: "poppins-bold",
+                  fontSize: 20,
+                  color: "#000",
+                }}
+              >
+                {capitalizeFirstLetter(userData?.username)}
+              </Text>
+              {userData?.profile?.country && (
+                <CountryFlag isoCode={userData.profile.country} size={16} />
+              )}
+            </View>
+
+            <Text
+              style={{
+                fontFamily: "poppins-semiBold",
+                fontSize: 14,
+                color: "#000",
+                marginBottom: 5,
+              }}
+            >
+              Level {calculateExp(userData?.profile?.experience || 0)}
+            </Text>
+
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+            >
+              <Ionicons name="ios-cash" size={20} color="#FFD700" />
+              <Text
+                style={{
+                  color: "#000", // Changed to black for better visibility
+                  fontFamily: "poppins-semiBold",
+                  fontSize: RFValue(12),
+                }}
+              >
+                {/* Placeholder value for Coins */}
+                1000
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        {/* Right Side - Arena Rating, Energy */}
+        <View style={{ alignItems: "flex-end" }}>
+          <View
+            style={{
+              paddingVertical: 5,
+              paddingHorizontal: 10,
+              borderRadius: 20,
+              marginBottom: 5,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <Ionicons name="trophy" size={20} color="#FDD92C" />
+              <Text
+                style={{
+                  // color: "#3F95F2",
+                  fontFamily: "poppins-semiBold",
+                  fontSize: RFValue(12),
+                }}
+              >
+                {userData.averageRating || 0}
+              </Text>
+            </View>
+          </View>
+
           <View
             style={{
               flexDirection: "row",
@@ -74,64 +146,20 @@ export default function UserProfile({ userData }) {
               gap: 5,
             }}
           >
+            <Ionicons name="flash" size={20} color="#32CD32" />
             <Text
               style={{
-                fontFamily: "poppins-bold",
-                fontSize: 20,
-                color: "#fff",
-                // textTransform: 'capitalize',
+                color: "#000", // Changed to black for better visibility
+                fontFamily: "poppins-semiBold",
+                fontSize: RFValue(12),
               }}
             >
-              {capitalizeFirstLetter(userData?.username)}
+              {/* Placeholder value for Energy */}
+              10/20
             </Text>
-            {userData?.profile?.country && (
-              <CountryFlag isoCode={userData.profile.country} size={16} />
-            )}
-          </View>
-
-          <Text
-            style={{
-              fontFamily: "poppins-semiBold",
-              fontSize: 14,
-              color: "#fff",
-              marginBottom: 5,
-            }}
-          >
-            Level {calculateExp(userData?.profile?.experience || 0)}
-          </Text>
-          <View
-            style={{
-              paddingVertical: 5,
-              paddingHorizontal: 10,
-              borderRadius: 20,
-              backgroundColor: "#EFF8FF",
-              marginTop: 5,
-            }}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
-                gap: 5,
-              }}
-            >
-              <Ionicons name="trophy" size={20} color="#FDD92C" />
-              <Text
-                style={{
-                  color: "#3F95F2",
-                  fontFamily: "poppins-semiBold",
-                  fontSize: RFValue(12),
-                }}
-              >
-                {userData.averageRating && (
-                  <>Arena Rating: {userData.averageRating || 0}</>
-                )}
-              </Text>
-            </View>
           </View>
         </View>
-      </LinearGradient>
+      </View>
     </View>
   );
 }
